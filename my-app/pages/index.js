0fe81087ae70b70616e49595b8ea179012a6b0e1
Gurkirat <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-05-20 21:19:05 +0530

    crone

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,13 +1,30 @@
 import Head from "next/head";
 
 import DataWidget from "../components/widget/DataWidget";
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 
 export default function Home() {
   const [val1, updateVal1] = useState(550);
   const [val2, updateVal2] = useState(102);
   const [val3, updateVal3] = useState(550);
   const [val4, updateVal4] = useState(550);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/get");
+        const jsonData = await response.json();
+        console.log(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+
+    const interval = setInterval(fetchData, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
       <div className="m-2" />
