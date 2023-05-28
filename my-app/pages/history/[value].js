22b4e92e9ffee0a95867ee54cebe403f701dc3dc
Gurkirat <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-05-28 16:42:57 +0530

    feat:graph

--- my-app/pages/history/[value].js
+++ my-app/pages/history/[value].js
@@ -1,10 +1,12 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { Chart } from "chart.js/auto";
 
 export default function HistoryPage() {
   const router = useRouter();
   const { value } = router.query;
   const [data, setData] = useState(null);
+  const chartRef = useRef(null);
 
   useEffect(() => {
     // Fetch data from the API endpoint
@@ -19,16 +21,45 @@ export default function HistoryPage() {
     fetchData();
   }, [value]);
 
+  useEffect(() => {
+    if (chartRef.current) {
+      chartRef.current.destroy();
+    }
+
+    if (data) {
+      // Get the latest 7 values
+      const latestData = data.slice(-7);
+
+      // Create a new chart
+      const ctx = document.getElementById("myChart").getContext("2d");
+      chartRef.current = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: latestData.map((entry) => entry.date),
+          datasets: [
+            {
+              data: latestData.map((entry) => entry.value),
+              label: "Value",
+              borderColor: "#3e95cd",
+              backgroundColor: "#7bb6dd",
+              fill: false,
+            },
+          ],
+        },
+      });
+    }
+  }, [data]);
+
   return (
     <div>
       <center>
-        <h1 className="text-3xl font-bold ">History Page of {value}</h1>
+        <h1 className="text-3xl font-bold">History Page of {value}</h1>
         {data ? (
           <div className="overflow-x-auto">
-            <table className="table ">
+            <table className="table">
               <thead>
                 <tr>
-                  <th></th>
+                  <th>#</th>
                   <th>Date</th>
                   <th>Value</th>
                 </tr>
@@ -36,18 +67,19 @@ export default function HistoryPage() {
               <tbody>
                 {data.map((entry, index) => (
                   <tr className="hover" key={index}>
-                    <th>{index + 1}</th>
-                    <td>Value: {entry.value}</td>
-                    <td>Date: {entry.date}</td>
+                    <th scope="row">{index + 1}</th>
+                    <td>{entry.date}</td>
+                    <td>{entry.value}</td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
         ) : (
-          <p>Loading...</p>
+          <p className="text-center">Loading...</p>
         )}
       </center>
+      <canvas id="myChart"></canvas>
     </div>
   );
 }
